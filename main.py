c5eab58e22437bd1c0de9e61c89278488609fee8
Kevin7744 <arapbiisubmissions@gmail.com>
2024-02-11 00:37:32 +0300

    Update

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-import os
+from os import environ
 from dotenv import load_dotenv
-
-load_dotenv()  
+from flask import Flask, jsonify, request
+# from flask_restful import reqparse, abort, Api, Resource
+from supabase.client import Client, create_client
 
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.pgvector import PGVector
 
 
-CONNECTION_STRING = os.getenv("SUPABASE_URL")
-
-# Instantiate OpenAI embeddings
-embeddings = OpenAIEmbeddings()
-
-def embed_text(text):
-    return embeddings.embed(text)
-
-db = PGVector(
-    connection_string=CONNECTION_STRING,
-    collection_name="documents" ,
-    embedding_function=embed_text
-)
-
-query = input("Enter your query: ")
+load_dotenv()  
 
 
-query_embedding = embeddings.embed(query)
 
-results = db.similarity_search_with_score(query_embedding)
+app = Flask(__name__)
 
-for doc, score in results:
-    print(f"Document content: {doc.pageContent}\nSimilarity score: {score:.4f}\n")
+
+
+TABLE = "documents"
+
+load_dotenv()
+
+url: str = environ.get("SUPABASE_URL")
+key: str = environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=environ.get("PORT", 5000), host='0.0.0.0')
