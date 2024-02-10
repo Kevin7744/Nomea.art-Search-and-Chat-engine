ef3a711a1274fbae5c71ca20c2e0f9ab3fb2b6f7
Kevin7744 <arapbiisubmissions@gmail.com>
2024-02-11 00:41:37 +0300

    connected instance

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 from os import environ
 from dotenv import load_dotenv
-from flask import Flask, jsonify, request
-# from flask_restful import reqparse, abort, Api, Resource
 from supabase.client import Client, create_client
-
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores.pgvector import PGVector
-
-
-load_dotenv()  
-
-
-
-app = Flask(__name__)
-
-
-
-TABLE = "documents"
 
 load_dotenv()
 
-url: str = environ.get("SUPABASE_URL")
-key: str = environ.get("SUPABASE_KEY")
+url = environ.get("SUPABASE_URL")
+key = environ.get("SUPABASE_SERVICE_KEY")
 
-supabase: Client = create_client(url, key)
+supabase = create_client(url, key)
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=environ.get("PORT", 5000), host='0.0.0.0')
+print("Connected to Supabase")
