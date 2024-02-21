bc718d12f864f6f8e8c034202e9bf4175bbb9ead
Kevin7744 <arapbiisubmissions@gmail.com>
2024-02-22 01:18:05 +0300

    Update

--- image_embeddings.py
+++ image_embeddings.py
@@ -5,6 +5,7 @@ from PIL import Image
 from io import BytesIO
 from sentence_transformers import SentenceTransformer
 import requests
+import json
 
 load_dotenv()
 
@@ -17,7 +18,7 @@ model = SentenceTransformer('clip-ViT-B-32')
 
 def download_and_update_images():
     response = supabase.table("images").select("id", "image_url").execute()
-    for record in response["data"]:
+    for record in response.data:  # Iterate over response.data instead of response["data"]
         image_id = record["id"]
         image_url = record["image_url"]
         full_url = f"https://imagedelivery.net/vfguozVHBGZa-6s8NQZayA/{image_url}/public"
@@ -28,9 +29,10 @@ def download_and_update_images():
         
         # Convert the image to embeddings using the pre-trained model
         image_embeddings = model.encode(image)
+        image_embeddings_list = image_embeddings.tolist()  # Convert ndarray to list
         
         # Update the `image_embeddings` column in the `posts` table
-        update_query = supabase.table("posts").update({"image_embeddings": image_embeddings}).eq("id", image_id).execute()
+        update_query = supabase.table("posts").update({"image_embeddings": image_embeddings_list}).eq("id", image_id).execute()
 
 # Example usage
 download_and_update_images()
